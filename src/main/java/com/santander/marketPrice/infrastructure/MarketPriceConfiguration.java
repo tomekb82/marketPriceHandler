b177a64845afc7b8793b590845148c920aa57af3
TB <TB@test.pl>
2023-05-17 01:52:37 +0200

    Market Price Handler Exercise

--- src/main/java/com/santander/marketPrice/infrastructure/MarketPriceConfiguration.java
+++ src/main/java/com/santander/marketPrice/infrastructure/MarketPriceConfiguration.java
@@ -22,18 +22,13 @@ class MarketPriceConfiguration {
     }
 
     @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setForceEncoding(true);
-        characterEncodingFilter.setEncoding("UTF-8");
-        registrationBean.setFilter(characterEncodingFilter);
-        return registrationBean;
+    PriceSubscriber priceSubscriber(CalculatingLatestPrice calculatingLatestPrice){
+        return new PriceSubscriber(calculatingLatestPrice);
     }
 
     @Profile("local")
     @Bean
-    CommandLineRunner init(CalculatingLatestPrice calculatingLatestPrice){
+    CommandLineRunner init(PriceSubscriber priceSubscriber, CalculatingLatestPrice calculatingLatestPrice){
         return args -> {
             String message =
 """
@@ -43,7 +38,7 @@ class MarketPriceConfiguration {
 109, GBP/USD, 1.2499,1.2561,01-06-2020 12:01:02:100
 110, EUR/JPY, 119.61,119.91,01-06-2020 12:01:02:110
 """;
-            calculatingLatestPrice.calculate(message);
+            priceSubscriber.onMessage(message);
             logger.info("Calculation of prizes completed");
             logger.info("Results:");
             logger.info(String.join("\n", calculatingLatestPrice.getAll().getPrices()
